ebe8327c6d9adf325d67ec1a51aad671c717203e
Alexandru Miron <alexandrumiron27@gmail.com>
2023-03-28 11:28:36 +0100

    added behaviour for optional tesselation and geometry shaders
    Added support for tesselation and geometry shaders in the shader class. These can now be added through a program in the shader class (optional),

--- render-engine/ShaderProgram.cpp
+++ render-engine/ShaderProgram.cpp
@@ -29,8 +29,8 @@ ShaderProgram::ShaderProgram(const char* vertexPath,
 	const char* fragmentPath,
 	const char* geometryPath,
 	const char* computePath,
-	const char* tessControlShader,
-	const char* tessEvalShader)
+	const char* tessControlPath,
+	const char* tessEvalPath)
 {
 	//Load and compile vertex shader
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -45,18 +45,70 @@ ShaderProgram::ShaderProgram(const char* vertexPath,
 	glCompileShader(fragmentShader);
 
 	//Optional shaders
-	GLuint tesselationControlShaderID = 0;
-	GLuint tesselationEvalShaderID = 0;
-	GLuint geometryShaderID = 0;
+	GLuint tesselationControlShader = 0;
+	GLuint tesselationEvalShader = 0;
+	GLuint geometryShader = 0;
 	GLuint computeShader = 0;
+
+	//if tesselation shaders are used
+	if (tessControlPath && tessEvalPath)
+	{
+		tesselationControlShader = glCreateShader(GL_TESS_CONTROL_SHADER);
+		const char* tescCode = readShaderCode(tessControlPath);
+		glShaderSource(tesselationControlShader, 1, &tescCode, NULL);
+		glCompileShader(tesselationControlShader);
+
+		tesselationEvalShader = glCreateShader(GL_TESS_EVALUATION_SHADER);
+		const char* teseCode = readShaderCode(tessEvalPath);
+		glShaderSource(tesselationEvalShader, 1, &teseCode, NULL);
+		glCompileShader(tesselationEvalShader);
+	}
+
+	if (geometryPath)
+	{
+		geometryShader = glCreateShader(GL_GEOMETRY_SHADER);
+		const char* geoCode = readShaderCode(geometryPath);
+		glShaderSource(geometryShader, 1, &geoCode, NULL);
+		glCompileShader(geometryShader);
+	}
+
+
 
 	//Link shaders
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
+	
+	if (tessControlPath && tessEvalPath)
+	{
+		glAttachShader(shaderProgram, tesselationControlShader);
+		glAttachShader(shaderProgram, tesselationEvalShader);
+	}
+
+	if (geometryPath)
+	{
+		glAttachShader(shaderProgram, geometryShader);
+	}
+
 	glLinkProgram(shaderProgram);
 
 	//Delete shaders
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+
+	if (tesselationControlShader != 0 && tesselationEvalShader != 0)
+	{
+		glDeleteShader(tesselationControlShader);
+		glDeleteShader(tesselationEvalShader);
+	}
+
+	if (geometryShader != 0)
+	{
+		glDeleteShader(geometryShader);
+	}
+}
+
+ShaderProgram::~ShaderProgram()
+{
+	glDeleteProgram(this->shaderProgram);
 }
--- render-engine/ShaderProgram.h
+++ render-engine/ShaderProgram.h
@@ -4,18 +4,20 @@
 
 #include <GL/glew.h>
 
-enum ShaderStage 
-{
-	VertexShader = 0,
-	FragmentShader = 1,
-	GeometryShader = 2,
-	ComputeShader = 3,
-	TessellationControlShader = 4,
-	TessellationEvaluationShader = 5
-};
+
 
 class ShaderProgram
 {
+	enum ShaderStage
+	{
+		VertexShader = 0,
+		FragmentShader = 1,
+		GeometryShader = 2,
+		ComputeShader = 3,
+		TessellationControlShader = 4,
+		TessellationEvaluationShader = 5
+	};
+
 private:
 	GLuint shaderProgram;
 	GLuint vertexShader;
@@ -28,6 +30,8 @@ public:
 		const char* fragmentPath,
 		const char* geometryPath = nullptr,
 		const char* computePath = nullptr,
-		const char* tessControlShader = nullptr,
-		const char* tessEvalShader = nullptr);
+		const char* tessControlPath = nullptr,
+		const char* tessEvalPath = nullptr);
+
+	~ShaderProgram();
 };
