908301a63b535b67eb12ec7180f2671a54f7b46e
SpartanPawnch <64585694+SpartanPawnch@users.noreply.github.com>
2023-04-11 17:35:30 +0300

    #12 - Component Script prototype

---
+++ docs/example_script.lua
@@ -0,0 +1,17 @@
+-- Component scripts are expected to return a table with
+-- the three functions described below. Naturally, local
+-- state can be declared before the return statement
+
+return{
+    -- Called on component creation
+    on_start=function ()
+    end,
+
+    -- Called each frame/tick
+    on_update=function ()
+    end,
+
+    -- Called on component destruction
+    on_delete=function ()
+    end
+}
--- test.lua
+++ test.lua
@@ -1,3 +1,3 @@
-logging.logInfo("Hello from Lua!\n")
+logging.logInfo("Hello from Lua!\n","Info 2\n")
 logging.logWarn("Warning from Lua!\n")
 logging.logErr("Error from Lua!\n")
