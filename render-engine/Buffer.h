bee93a1845ce806334a9ea1a2150df76168b7ea5
Alexandru Miron <alexandrumiron27@gmail.com>
2023-04-11 15:07:47 +0100

    added Buffer class

---
+++ render-engine/Buffer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+
+class VertexBuffer
+{
+private:
+	GLuint vertexBuffer;
+
+public:
+	void CreateBuffer(GLsizeiptr bufferSize_B, const void* data);
+
+
+};
+
+class IndexBuffer
+{
+private:
+	GLuint indexBuffer;
+
+public:
+	void CreateBuffer(GLsizeiptr bufferSize_B, const void* data);
+
+
+};
